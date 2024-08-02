119cd9cda602485f5ba37feedab3d27f10f98ceb
cornelia <corneliakelinske@gmail.com>
2024-08-02 07:47:41 -0700

    add event migration

---
+++ priv/repo/migrations/20240802171522_create_events.exs
@@ -0,0 +1,15 @@
+defmodule WomenInTechVic.Repo.Migrations.CreateEvents do
+  use Ecto.Migration
+
+  def change do
+    create table(:events) do
+      add :title, :text, null: false
+      add :scheduled_at, :utc_datetime_usec, null: false
+      add :online, :boolean, null: false
+      add :address, :text, null: false
+      add :description, :text, null: false
+
+      timestamps(type: :utc_datetime_usec)
+    end
+  end
+end
