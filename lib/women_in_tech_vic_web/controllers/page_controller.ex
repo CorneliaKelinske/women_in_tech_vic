c339814affdd1f0a9f413b502ec1dcb3380445fd
cornelia <corneliakelinske@gmail.com>
2024-08-06 10:12:48 -0700

    display event

--- lib/women_in_tech_vic_web/controllers/page_controller.ex
+++ lib/women_in_tech_vic_web/controllers/page_controller.ex
@@ -1,9 +1,19 @@
 defmodule WomenInTechVicWeb.PageController do
   use WomenInTechVicWeb, :controller
 
+  alias WomenInTechVic.Content
+
   def home(conn, _params) do
+    online_event =
+      %{online: true, order_by: :scheduled_at, limit: 1}
+      |> Content.all_events()
+      |> case do
+        [] -> "No event scheduled"
+        [event] -> event
+      end
+
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home, layout: false, event: online_event)
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,33 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     WomenInTechVic.Repo.insert!(%WomenInTechVic.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+if Mix.env() === :dev do
+  alias WomenInTechVic.Content
+
+  Content.create_event(%{
+    title: "Bi-weekly online meeting",
+    scheduled_at: "2024-08-14T00:00:00Z",
+    online: true,
+    address: "meet.google.com/uam-eyys-bxs",
+    description: "Our bi-weekly online meetup where we can hang out and chat.
+                  This meeting is one of the more technical, code-heavy meetings. We'll continue exploring RUST. During the last technical meeting, we did a basic initial exploration using https://exercism.org/tracks/rust.
+                  This time, we'll be a bit more hands-on. We are going to use
+                  https://github.com/rust-lang/rustlings
+                  To make this work on your computer, you'd need a code editor (e.g., VS code), clone our copy of the Rustlings repo at https://github.com/CorneliaKelinske/rustlings_meetup and install Rust plus Rustlings. Install instructions are linked in the Rustling GitHub Repo.
+                  Let me (Cornelia) know if you need help with the installation or if there are any questions/concerns.
+                  If this seems intimidating, don't worry! We can help you get set up during the meet-up, plus we'll have someone screen share so we can work through the exercises together.
+                  "
+  })
+
+  Content.create_event(%{
+    title: "Bi-weekly online meeting",
+    scheduled_at: "2024-08-28T00:00:00Z",
+    online: true,
+    address: "meet.google.com/uam-eyys-bxs",
+    description: "Our bi-weekly online meetup where we can hang out and chat.
+                  This meeting is one of the more technical, code-heavy meetings. We'll continue exploring RUST. During the last technical meeting, we did a basic initial exploration using https://exercism.org/tracks/rust.
+                  This time, we'll be a bit more hands-on. We are going to use
+                  https://github.com/rust-lang/rustlings
+                  To make this work on your computer, you'd need a code editor (e.g., VS code), clone our copy of the Rustlings repo at https://github.com/CorneliaKelinske/rustlings_meetup and install Rust plus Rustlings. Install instructions are linked in the Rustling GitHub Repo.
+                  Let me (Cornelia) know if you need help with the installation or if there are any questions/concerns.
+                  If this seems intimidating, don't worry! We can help you get set up during the meet-up, plus we'll have someone screen share so we can work through the exercises together.
+                  "
+  })
+end
