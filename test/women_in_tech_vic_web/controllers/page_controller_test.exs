fff2a8a86d1c3e44af7c389c46650992f4d4976d
cornelia <corneliakelinske@gmail.com>
2024-08-06 10:12:48 -0700

    display event

--- test/women_in_tech_vic_web/controllers/page_controller_test.exs
+++ test/women_in_tech_vic_web/controllers/page_controller_test.exs
@@ -1,8 +1,26 @@
 defmodule WomenInTechVicWeb.PageControllerTest do
   use WomenInTechVicWeb.ConnCase
 
+  import WomenInTechVic.Support.Factory, only: [insert: 1, build: 2]
+  alias WomenInTechVic.Content.Event
+
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ ">Welcome to Women in Tech Victoria</h1>\n"
+  end
+
+  test "shows message when no meeting has been scheduled", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "No event scheduled"
+  end
+
+  test "shows event title when meeting has been scheduled", %{conn: conn} do
+    :online_event
+    |> build(title: "Bi-weekly online meeting")
+    |> then(&struct!(Event, &1))
+    |> insert()
+
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Bi-weekly online meeting"
   end
 end
