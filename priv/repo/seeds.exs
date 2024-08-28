2faee6a5bee0a6176902c345a7b8ac82b344b000
cornelia <corneliakelinske@gmail.com>
2024-08-28 13:36:53 -0700

    update seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -22,13 +22,12 @@ if Mix.env() !== :test do
     online: true,
     address: "https://meet.google.com/uam-eyys-bxs",
     description: "Our bi-weekly online meetup where we can hang out and chat.
-                  This meeting is one of the more technical, code-heavy meetings. We'll continue exploring RUST. During the last technical meeting, we did a basic initial exploration using https://exercism.org/tracks/rust.
-                  This time, we'll be a bit more hands-on. We are going to use
-                  https://github.com/rust-lang/rustlings
-                  To make this work on your computer, you'd need a code editor (e.g., VS code), clone our copy of the Rustlings repo at https://github.com/CorneliaKelinske/rustlings_meetup and install Rust plus Rustlings. Install instructions are linked in the Rustling GitHub Repo.
-                  Let me (Cornelia) know if you need help with the installation or if there are any questions/concerns.
-                  If this seems intimidating, don't worry! We can help you get set up during the meet-up, plus we'll have someone screen share so we can work through the exercises together.
-                  "
+    This meeting is one of the more technical, code-heavy meetings. We are going to use
+    https://github.com/rust-lang/rustlings. To make this work on your computer, you will need a code editor
+    (e.g., VS code) . The Rustlings repo linked above has excellent instructions for what to do to get set up.
+    Follow those, and you are good to go!
+    Let me (Cornelia) know if you need help with the installation or have any questions or concerns.
+    If this seems intimidating, don't worry! We can help you get set up during the meet-up, plus we'll have someone screen share so we can work through the exercises together."
   })
 
   Content.create_event(%{
@@ -48,12 +47,11 @@ if Mix.env() !== :test do
     online: true,
     address: "https://meet.google.com/uam-eyys-bxs",
     description: "Our bi-weekly online meetup where we can hang out and chat.
-                  This meeting is one of the more technical, code-heavy meetings. We'll continue exploring RUST. During the last technical meeting, we did a basic initial exploration using https://exercism.org/tracks/rust.
-                  This time, we'll be a bit more hands-on. We are going to use
-                  https://github.com/rust-lang/rustlings
-                  To make this work on your computer, you'd need a code editor (e.g., VS code), clone our copy of the Rustlings repo at https://github.com/CorneliaKelinske/rustlings_meetup and install Rust plus Rustlings. Install instructions are linked in the Rustling GitHub Repo.
-                  Let me (Cornelia) know if you need help with the installation or if there are any questions/concerns.
-                  If this seems intimidating, don't worry! We can help you get set up during the meet-up, plus we'll have someone screen share so we can work through the exercises together.
-                  "
+    This meeting is one of the more technical, code-heavy meetings. We are going to use
+    https://github.com/rust-lang/rustlings. To make this work on your computer, you will need a code editor
+    (e.g., VS code) . The Rustlings repo linked above has excellent instructions for what to do to get set up.
+    Follow those, and you are good to go!
+    Let me (Cornelia) know if you need help with the installation or have any questions or concerns.
+    If this seems intimidating, don't worry! We can help you get set up during the meet-up, plus we'll have someone screen share so we can work through the exercises together."
   })
 end
