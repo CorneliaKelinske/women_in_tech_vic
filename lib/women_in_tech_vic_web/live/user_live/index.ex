e0cebabeb110fb31f6dddcaeb72159454a2dbe58
cornelia <corneliakelinske@gmail.com>
2024-11-07 13:40:38 -0800

    edit_role_option add edit option for admin hacky way

--- lib/women_in_tech_vic_web/live/user_live/index.ex
+++ lib/women_in_tech_vic_web/live/user_live/index.ex
@@ -11,7 +11,8 @@ defmodule WomenInTechVicWeb.UserLive.Index do
     {:ok,
      socket
      |> assign_title(@title)
-     |> assign_users()}
+     |> assign_users()
+     |> assign(show_modal: false)}
   end
 
   @impl true
@@ -21,6 +22,42 @@ defmodule WomenInTechVicWeb.UserLive.Index do
       _ -> {:noreply, put_flash(socket, :error, "Could not delete user")}
     end
   end
+
+  # coveralls-ignore-start
+  @impl true
+  def handle_event("open-role-modal", %{"id" => user_id}, socket) do
+    user = Accounts.get_user!(String.to_integer(user_id))
+
+    {:noreply,
+     socket
+     |> assign(show_modal: true)
+     |> assign(selected_user: user)}
+  end
+
+  @impl true
+  def handle_event("close-modal", _, socket) do
+    {:noreply, assign(socket, show_modal: false)}
+  end
+
+  @impl true
+  def handle_event("save-role", %{"role" => role, "user_id" => user_id}, socket) do
+    case Accounts.update_user_role(String.to_integer(user_id), role) do
+      {:ok, %User{}} ->
+        {:noreply,
+         socket
+         |> assign(show_modal: false)
+         |> assign_users()
+         |> put_flash(:info, "User role updated successfully")}
+
+      _ ->
+        {:noreply,
+         socket
+         |> assign(show_modal: false)
+         |> put_flash(:error, "Could not update user role")}
+    end
+  end
+
+  # coveralls-ignore-stop
 
   defp assign_title(socket, title), do: assign(socket, title: title)
 
